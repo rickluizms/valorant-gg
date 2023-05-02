18b807929228c9073bc7a662c2c034eb2c1bf0af
rickluiz <rickluizms@gmail.com>
2023-05-02 01:32:26 -0300

    update commit

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def get_overview():
     
     services.scrap.get_overview(pacific)
 
-
 def get_stats():
 
     services.scrap.get_stats(americas)
@@ -26,13 +25,21 @@ def get_matches():
 
     services.scrap.get_matches(americas)
 
+    services.scrap.get_matches(emea)
+
+    services.scrap.get_matches(pacific)
+
 def get_agents():
     
     services.scrap.get_agents(americas)
 
+    services.scrap.get_agents(emea)
+
+    services.scrap.get_agents(pacific)
+
 #get_overview()
 #get_stats()
-#get_matches()
+get_matches()
 get_agents()
 
 
